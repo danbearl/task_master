4e126ddf776e3e6632c9c69a1cc105ac86641036
Dan Bearl <dan.bearl@gmail.com>
2015-02-11 21:13:20 -0500

    Updating page routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,55 +1,19 @@
 TaskMaster::Application.routes.draw do
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
 
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
+  root 'pages#index'
 
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
   get 'log_in' => 'sessions#new'
   get 'log_out' => 'sessions#destroy'
+
   resources :users
   resources :sessions
 
-  resources :pages, only: [:new, :create]
+  resources :pages, only: [:index, :new, :create]
 
   #keep these at the bottom of your file. They should be the last routes.
   get "/:slug", to: "pages#show", as: :slug
   get "/:slug/edit", to: "pages#edit", as: :edit_slug
   put "/:slug", to: "pages#update", as: :update_slug
   post "/:slug", to: "pages#destroy", as: :destroy_slug
+
 end
